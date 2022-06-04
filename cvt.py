7fad3182189e97f51513ed2a11221374f63041fe
Brian90709 <76473884+Brian90709@users.noreply.github.com>
2022-06-04 14:51:09 +0800

    Update cvt.py

--- cvt.py
+++ cvt.py
@@ -3,7 +3,7 @@ vidcap = cv2.VideoCapture('train.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("frames/%05d.jpg" % count, image)     # save frame as JPEG file      
+  cv2.imwrite("frames/%05d.jpg" % count, image)     # save frame as JPG file      
   success,image = vidcap.read()
   print('Read a new frame: ', success)
   count += 1
